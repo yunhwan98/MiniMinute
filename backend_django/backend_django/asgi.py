7247e2ee30589d344bd03ce182adf0f17b1e0aeb
GGANCC1 <tmddn9803@naver.com>
2022-03-31 15:30:29 +0900

    장고프로젝트 생성

--- backend_django/backend_django/asgi.py
+++ backend_django/backend_django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for back_end_django project.
+ASGI config for backend_django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back_end_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_django.settings')
 
 application = get_asgi_application()
--- backend_django/backend_django/urls.py
+++ backend_django/backend_django/urls.py
@@ -1,4 +1,4 @@
-"""back_end_django URL Configuration
+"""backend_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
