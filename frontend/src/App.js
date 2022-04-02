c351337ab9a6cb47dc6cdf094b7baccecec0229f
jeewonMoon <papers1210@kakao.com>
2022-04-02 12:33:33 +0900

    페이지 라우팅
    - components, pages 디렉토리 생성
    - 부트스트랩, react-router-dom 설치
    - 페이지 라우팅 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import logo from './logo.svg';
+import Start from './pages/Start';
+import Home from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Start />} />
+                    <Route path="/home" element={<Home />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
