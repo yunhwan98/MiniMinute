500d91019c71025325eb03db34509506c37088e2
jeewonMoon <papers1210@kakao.com>
2022-04-04 21:47:03 +0900

    사이드바 수정
    - 설계에 맞춰서 사이드 바 수정

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link } from 'react-router-dom';
-import profile from '../images/profile.png';
+import profile from '../images/profile3.png';
+import setting from '../images/setting.png';
+import {Modal} from "react-bootstrap";
 
 function Sidebar() {
+    const dropDownRef = useRef(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [logShow, setLogShow] = useState(false);
+    const [dirShow, setDirShow] = useState(false);
+
     return (
         <div className="sidebar">
+            <div className="dropdown">
+                <button className="setting-btn"
+                    onClick={() => setIsOpen(!isOpen)}>
+                    <img src={setting} style={{height: "20px"}} />
+                </button>
+                <ul
+                    ref={dropDownRef}
+                    className={`menu ${isOpen ? 'active' : 'inactive'}`}>
+                    <li><Link className="dropdown-item" to="">프로필 수정</Link></li>
+                    <li><Link className="dropdown-item" to="/">로그아웃</Link></li>
+                </ul>
+            </div>
             <div className="profile">
                 <img src={profile} style={{height: "140px"}}/>
                 <h4>Username</h4>
-                <button type="button" className="profile-btn">프로필 변경</button>
+                <button type="button" className="new-btn" onClick={() => setLogShow(true)}>새 회의록</button>
+                <Modal show={logShow} onHide={() => setLogShow(false)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>회의록 생성</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <button type="button" class="btn btn-primary" onClick={() => setLogShow(false)}>
+                            생성
+                        </button>
+                    </Modal.Footer>
+                </Modal>
             </div>
             <ul className="directory">
                 <li>
@@ -61,13 +92,29 @@ function Sidebar() {
                         <Link to ="" className="directory-link">학교</Link>
                     </li>
                     <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
-                             className="bi bi-folder-plus" viewBox="0 2 18 18">
-                            <path
-                                d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z"/>
-                            <path
-                                d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"/>
-                        </svg>
+                        <button type="button" className="setting-btn" onClick={() => setDirShow(true)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
+                                 className="bi bi-folder-plus" viewBox="0 2 18 18">
+                                <path
+                                    d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z"/>
+                                <path
+                                    d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"/>
+                            </svg>
+                        </button>
+                        <Modal show={dirShow} onHide={() => setDirShow(false)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>폴더 추가</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h6>폴더 이름</h6>
+                        <input type="text" className="form-control" id="directory-name" />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button type="button" class="btn btn-primary" onClick={() => setDirShow(false)}>
+                            추가
+                        </button>
+                    </Modal.Footer>
+                </Modal>
                     </li>
                 </ul>
             </ul>
