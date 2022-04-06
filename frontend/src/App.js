424339228d75efd8147d588b1bb782c9a0ac3136
jeewonMoon <papers1210@kakao.com>
2022-04-06 22:41:51 +0900

    회의록 목록 페이지 구현
    - 사이드 바의 '내 회의록' 하위 폴더 클릭 시 나타날 페이지
    - 화면 구성이 똑같은 검색 페이지도 만들었음

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Home from './pages/Home';
 import Start_page from './pages/Start_page';
 import './pages/Start_page.css';
 import Login from './components/Login_modal';
+import Log_list from './pages/Log_list'
+import Search from './pages/Search'
 
 function App(){
     return (
@@ -16,6 +18,8 @@ function App(){
                     <Route path="/home" element={<Home />} />
                     <Route path="/start" element={<Start_page />} />    //초기화면 진입
                     <Route path="/login" element={<Login />} />
+                    <Route path="/loglist" element={<Log_list />} />
+                    <Route path="/search" element={<Search />} />
                 </Routes>
             </div>
         </BrowserRouter>
