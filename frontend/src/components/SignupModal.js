9c57dcde3323ff9fda194687338e3cf6b763a5fe
yunhwan98 <79241793+yunhwan98@users.noreply.github.com>
2022-04-14 04:01:59 +0900

    로그인, 회원가입 구현 axios , react-google-login , gapi-script  (npm설치) 비밀번호 찾기 페이지 (기능 X) 로그인, 회원가입 기능 구현 로그인, 회원가입 시 오류 나타냄 구글 로그인 시 토큰 받기 (기능 X) css 수정

--- frontend/src/components/SignupModal.js
+++ frontend/src/components/SignupModal.js
@@ -1,47 +1,134 @@
 import React, {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Modal, Button} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import LoginGoogle from './LoginGoogle';
 
-
+const clientId = "1064677249953-799g6ker89ntqd3kfq2kpce60saut59u.apps.googleusercontent.com"
 export default function SignupModal(props) {
   const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
+  const [errormsg, setErrormsg] = useState("");
+  const [name, setname] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+  const [color, setColor] = useState("red")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [accessToken,setaccessToken] = useState('');
+  const handleClose = () => {setShow(false); setErrormsg(''); setname(''); setEmail(''); setPassword(''); setConfirmPassword('');}
   const handleShow = () => setShow(true);
-  const {name} = props; 
+
+  const onNameHandler = (event) => {
+    setname(event.currentTarget.value)
+  }
+  const onEmailHandler = (event) => {
+      setEmail(event.currentTarget.value)
+  }
+  const onPasswordHandler = (event) => {
+      setPassword(event.currentTarget.value)
+  }
+  const onConfirmPasswordHandler = (event) => {
+      setConfirmPassword(event.currentTarget.value)
+  }
+  
+  //회원가입 버튼 누르면 정보 전달
+  const onSubmit = async(event) => {  
+
+      event.preventDefault();
+      axios.post(     
+          "http://127.0.0.1:8000/rest-auth/signup",{
+            "username" : name,
+            "email" : email,
+            "password1" : password,
+            "password2" : confirmPassword
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          handleClose();
+          alert('회원가입이 되었습니다.')
+        })
+        .catch((error) => { //오류메시지 보이게 함
+          setColor("red");
+          console.log(error.response);
+          console.log(error.response.data.email);
+          if(error.response.data.email == 'A user is already registered with this e-mail address.'){
+            setErrormsg("※ 중복된 이메일 입니다 ※");
+      
+          }
+          else if(password !== confirmPassword) {
+            setErrormsg("※ 비밀번호를 확인해주세요 ※");
+          }
+          else if(password == 'This password is too common.', 'This password is entirely numeric.'){
+            setErrormsg("※ 특수문자를 포함한 8자 이상의 비밀번호를 입력해주세요 ※");
+          }
+        });
+    
+  }
+  //중복확인 버튼 누르면 중복이메일인지 체크
+  const checkEmail = async(event) => {  
+
+    event.preventDefault();
+    axios.post(     
+        "http://127.0.0.1:8000/rest-auth/signup",{
+          "email" : email
+        }
+      )
+      .then((response) => {
+        
+      })
+      .catch((error) => { //오류메시지 보이게 함
+        console.log(error.response);
+        console.log(error.response.data.email);
+        setColor("red");
+        if(!error.response.data.email){
+          setColor("rgb(65, 187, 81)");
+          setErrormsg("사용가능한 이메일 입니다!");
+        }
+        else if(error.response.data.email == 'A user is already registered with this e-mail address.'){
+          setErrormsg("※ 중복된 이메일 입니다 ※");
+        }
+        else{
+          setErrormsg("※ 유효하지 않은 이메일 형식 입니다 ※");
+        }
+      });
+  
+}
+
+
+
+  const {title} = props; 
+  
   return (
     <>
+
       <Button className="nextButton" id="Signup-Button" onClick={handleShow}>
-        {name}
+        {title}
       </Button>
       <Modal show={show} onHide={handleClose} centered>
-      <div class="signup-form">    
+      <div className="signup-form">    
         <Modal.Header closeButton>
           <Modal.Title>회원가입</Modal.Title>
         </Modal.Header>          
-            <form>
-                <li>
-                    <label>닉네임</label> <input type="text" name="nickname" class="signup-text" placeholder="닉네임"></input>
-                    <button class="submit-btn" id="nickname_check">중복확인</button>
-                </li>              
-                <li>
-                    <label>이메일</label> <input type="text" name="email" class="signup-text" placeholder="이메일"></input>
-                </li>
-                <li>
-                    <label>비밀번호</label> <input type="password" name="password" class="signup-text" placeholder="비밀번호"></input>
-                </li>
-                <li>
-                    <label>비밀번호 확인</label> <input type="password" name="password_check" class="signup-text" placeholder="비밀번호 확인"></input>                   
-                </li>
-
-                <input type="submit" value="회원가입" class="submit-btn"></input>
-                <hr className="signup-hr"/>
-                <div id="google-title">구글 아이디로 회원가입</div>
-            </form>
-           
+            <form  onSubmit={onSubmit}>
+              <div className="Errormsg" style={{backgroundColor : color}}>{errormsg} </div>
+              <div><label>이메일</label> <input name="email" type="email" placeholder="이메일" value={email} onChange={onEmailHandler} className="signup-text"/>
+              <button type="button" onClick={checkEmail} className="submit-btn" id="email_check">중복 확인</button></div>
+              <div><label>닉네임</label> <input name="name" type="text" placeholder="닉네임" value={name} onChange={onNameHandler} className="signup-text"/></div>                    
+              <div><label>비밀번호</label> <input name="password" type="password" placeholder="비밀번호" value={password} onChange={onPasswordHandler} className="signup-text"/></div>
+              <div><label>비밀번호 확인</label> <input name="confirmPassword" type="password" placeholder="비밀번호 확인" value={confirmPassword} onChange={onConfirmPasswordHandler} className="signup-text"/></div>
+              <div><button type="submit" onSubmit={onSubmit} className="submit-btn">회원가입</button></div>
+              <hr className="signup-hr"/>
+              <div id="google-title">구글 아이디로 회원가입</div>
+              <div className ="google-login">
+                <LoginGoogle setaccessToken={setaccessToken} handleClose={handleClose} />
+                </div>
+            </form> 
         </div>
       </Modal>
+
     </>
+    
   );
 }
 
--- frontend/src/components/Start_nav.js
+++ frontend/src/components/Start_nav.js
@@ -1,32 +1,62 @@
 import React from "react";
-import { Link, renderMatches } from 'react-router-dom';
+import { Link, renderMatches, useNavigate } from 'react-router-dom';
 import LoginModal from './LoginModal';
 import SignupModal from './SignupModal';
+import miniminute_logo from '../images/logo.png';
+import {Button} from 'react-bootstrap';
 
-
-
-const Start_nav = () => {
+const Start_nav = (props) => {
+  const  navigate = useNavigate();
+  const onClick = (event) => {  //홈으로 이동
+    navigate('/home');
+  }
+  const Logout = (e) => {   //로그아웃 실행
+    console.log('로그아웃 실행')
+    props.userLogout();
+  }
+    if(!props.isAuthenticated){ //토큰이 없을 때 화면(인증 X)
     return (
 
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Mini Minute</a>     
-
+        <div className="container-fluid">  
+          <img src={miniminute_logo} style={{height: "60px"}} />
           <div className="collapse navbar-collapse" id="navbarColor03">
             <ul className="navbar-nav me-auto">          
             </ul>
 
             <form className="d-flex">
 
-              <LoginModal name="로그인"/>
+              <LoginModal title="로그인" userHasAuthenticated={props.userHasAuthenticated}/>
               <ul></ul>
-              <SignupModal name="회원가입"/>
+              <SignupModal title="회원가입"/>
 
             </form>
             </div>
             </div>
         </nav>
     );
+    }
+    else{ //토큰이 있을 때 화면(인증)
+      return(
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">  
+        <img src={miniminute_logo} style={{height: "60px"}} />
+        <div className="collapse navbar-collapse" id="navbarColor03">
+          <ul className="navbar-nav me-auto">          
+          </ul>
+
+          <form className="d-flex">
+              <Button className="nextButton" id="Login-Button" onClick={onClick}>내 회의록</Button>
+              <ul></ul>
+              <Button className="nextButton" id="Login-Button" onClick={Logout}>로그아웃</Button>
+
+          </form>
+          </div>
+          </div>
+      </nav>
+      );
+
+    }
 };
 
 export default Start_nav;
