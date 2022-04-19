7fc9ae6c4d756006f35bb158a9a31dd84a197fbb
HSY <hs03130@outlook.kr>
2022-04-20 01:18:09 +0900

    CHARSET 수정

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -120,7 +120,11 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '1234',  # mariaDB 설치 시 입력한 root 비밀번호 입력
         'HOST': 'localhost',
-        'PORT': '3306'
+        'PORT': '3306',
+        'TEST': {
+            'CHARSET': 'utf8mb4',
+            'COLLATION': 'utf8mb4_unicode_ci',
+        }
     }
 }
 
