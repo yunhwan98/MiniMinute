cae95ad57f431a0bd5b2216d6719b6862535d58a
yunhwan98 <79241793+yunhwan98@users.noreply.github.com>
2022-05-13 00:51:50 +0900

    setting

--- backend_django/voice_recognition/urls.py
+++ backend_django/voice_recognition/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index),
 
     path('recognition/lists/<int:mn_id>', views.voice_recognition_list),
-    path('recognition', views.voice_recognition),
+    path('recognition/<int:mn_id>/<int:vr_seq>', views.voice_recognition),
+    path('recognition/<int:mn_id>/search', views.voice_recognition_search),
+
 ]
