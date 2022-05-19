3a473458831afd0778797286ecc2e4b1ef58a879
hs03130 <z@26501340>
2022-05-19 13:35:51 +0900

    파일 업로드 수정, job name 수정, 화자1명일때 오류 수정

--- backend_django/voice_recognition/views.py
+++ backend_django/voice_recognition/views.py
@@ -14,7 +14,7 @@ from minute.serializers import SpeakerSerializer
 from django.db.models import Q
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
-from urllib import request
+from urllib import request as urllib_request
 from ast import literal_eval
 from botocore.config import Config
 import boto3
@@ -32,9 +32,9 @@ def index(request):
 
 
 # AWS transcribe 호출
-def transcribe_file(file_nm, user_id, speaker_cnt):
+def transcribe_file(file, speaker_cnt):
     bucket = 'miniminute-bucket'
-    job_name = '{}_{}'.format(user_id, file_nm)
+    job_name = '{}_{}.{}'.format(file.file_id, file.file_name, file.file_extension)
 
     # Transcribe를 위한 Config 설정
     my_config = Config(
@@ -50,17 +50,28 @@ def transcribe_file(file_nm, user_id, speaker_cnt):
     transcribe = boto3.client('transcribe', config=my_config)
 
     # s3에 업로드한 파일 URL
-    job_uri = 'https://s3.ap-northeast-2.amazonaws.com/{}/{}.wav'.format(bucket, job_name)
-    transcribe.start_transcription_job(
-        TranscriptionJobName=job_name,
-        Media={'MediaFileUri': job_uri},
-        MediaFormat='wav',
-        LanguageCode='ko-KR',
-        Settings={
-            'ShowSpeakerLabels': True,  # 화자분리 기능 True or False
-            'MaxSpeakerLabels': speaker_cnt  # 화자수
-        }
-    )
+    job_uri = 'https://s3.ap-northeast-2.amazonaws.com/{}/{}'.format(bucket, job_name)
+    if speaker_cnt == 1 :
+        transcribe.start_transcription_job(
+            TranscriptionJobName=job_name,
+            Media={'MediaFileUri': job_uri},
+            MediaFormat=file.file_extension,
+            LanguageCode='ko-KR',
+            Settings={
+                'ShowSpeakerLabels': False
+            }
+        )
+    else :
+        transcribe.start_transcription_job(
+            TranscriptionJobName=job_name,
+            Media={'MediaFileUri': job_uri},
+            MediaFormat=file.file_extension,
+            LanguageCode='ko-KR',
+            Settings={
+                'ShowSpeakerLabels': True,  # 화자분리 기능 True or False
+                'MaxSpeakerLabels': speaker_cnt  # 화자수
+            }
+        )
 
     # Transcribe job 작업이 끝나면 결과값 불러옴
     while True:
@@ -68,17 +79,14 @@ def transcribe_file(file_nm, user_id, speaker_cnt):
         if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
             save_json_uri = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
             break
+
     # Transcribe 결과가 저장된 웹주소
     save_json_uri = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
 
     # 웹서버 결과 파이썬으로 불러오기
-    load = request.urlopen(save_json_uri)
-    confirm = load.status
+    load = urllib_request.urlopen(save_json_uri)
+    # confirm = load.status
     results = load.read().decode('utf-8')
-
-    # 문자열을 딕셔너리로 변환 후 결과 가져오기
-    transcribe_text = literal_eval(results)['results']['transcripts'][0]['transcript']
-    # print(json.dumps(literal_eval(results), indent=2))
     return literal_eval(results)
 
 
@@ -96,50 +104,75 @@ def voice_recognition_list(request, mn_id):
         return JsonResponse(serializer.data, safe=False)
 
     elif request.method == 'POST':
-        results = transcribe_file(file.file_name, request.user.id, request.data.get('speaker_cnt'))
+        results = transcribe_file(file, request.data.get('speaker_cnt'))
 
         # 화자 생성
         speaker_dic = {}
-        for i in range(0, results['results']['speaker_labels']['speakers']):
-            speaker_data = {'speaker_name': 'spk_' + str(i), 'mn_id': mn_id}
+        if request.data.get('speaker_cnt') == 1:
+            speaker_data = {'speaker_name':'spk_0', 'mn_id':mn_id}
             speaker_serializer = SpeakerSerializer(data=speaker_data)
             if speaker_serializer.is_valid():
                 speaker_serializer.save()
-                speaker_dic[i] = speaker_serializer.data['speaker_seq']
+                speaker_dic[0] = speaker_serializer.data['speaker_seq']
+        else:
+            for i in range(0, results['results']['speaker_labels']['speakers']):
+                speaker_data = {'speaker_name': 'spk_' + str(i), 'mn_id': mn_id}
+                speaker_serializer = SpeakerSerializer(data=speaker_data)
+                if speaker_serializer.is_valid():
+                    speaker_serializer.save()
+                    speaker_dic[i] = speaker_serializer.data['speaker_seq']
 
         # 음성 인식 생성
         seq = 0
         item_seq = 0
         max_item_len = len(results['results']['items'])
-        for segment in results['results']['speaker_labels']['segments']:
-            # vr_text 구하기
-            item_len = len(segment['items'])
+        if request.data.get('speaker_cnt') == 1:
             vr_text = []
-            while (item_len > 0):
-                if (results['results']['items'][item_seq]['type'] == 'punctuation'):
-                    item_len += 1
-                vr_text.append(results['results']['items'][item_seq]['alternatives'][0]['content'])
+            for item in results['results']['items']:
+                vr_text.append(item['alternatives'][0]['content'])
                 vr_text.append(' ')
+                if item['type'] == 'punctuation':
+                    vr_text = ''.join(vr_text)
+                    vr_data = {'vr_seq': seq, 'vr_start': results['results']['items'][0]['start_time'], 'vr_end': results['results']['items'][item_seq-1]['end_time'],
+                                   'vr_text': vr_text, 'mn_id': mn_id, 'speaker_seq': speaker_dic[0]}
+                    vr_serializer = VoiceRecognitionSerializer(data=vr_data)
+                    if vr_serializer.is_valid():
+                        vr_serializer.save()
+                        vr_text = []
+                        seq += 1
+                    else:
+                        return JsonResponse(vr_serializer.errors, status=400)
                 item_seq += 1
-                item_len -= 1
-            if (item_seq < max_item_len and results['results']['items'][item_seq]['type'] == 'punctuation'):
-                vr_text.append(results['results']['items'][item_seq]['alternatives'][0]['content'])
-                item_seq += 1
-            vr_text = ''.join(vr_text)
+        else:
+            for segment in results['results']['speaker_labels']['segments']:
+                # vr_text 구하기
+                item_len = len(segment['items'])
+                vr_text = []
+                while (item_len > 0):
+                    if (results['results']['items'][item_seq]['type'] == 'punctuation'):
+                        item_len += 1
+                    vr_text.append(results['results']['items'][item_seq]['alternatives'][0]['content'])
+                    vr_text.append(' ')
+                    item_seq += 1
+                    item_len -= 1
+                if (item_seq < max_item_len and results['results']['items'][item_seq]['type'] == 'punctuation'):
+                    vr_text.append(results['results']['items'][item_seq]['alternatives'][0]['content'])
+                    item_seq += 1
+                vr_text = ''.join(vr_text)
 
-            vr_data = {'vr_seq': seq, 'vr_start': segment['start_time'], 'vr_end': segment['end_time'],
-                       'vr_text': vr_text, 'mn_id': mn_id,
-                       'speaker_seq': speaker_dic[int(segment['speaker_label'][4:])]}
-            vr_serializer = VoiceRecognitionSerializer(data=vr_data)
-            if vr_serializer.is_valid():
-                vr_serializer.save()
-            else:
-                return JsonResponse(vr_serializer.errors, status=400)
-            seq += 1
+                vr_data = {'vr_seq': seq, 'vr_start': segment['start_time'], 'vr_end': segment['end_time'],
+                           'vr_text': vr_text, 'mn_id': mn_id,
+                           'speaker_seq': speaker_dic[int(segment['speaker_label'][4:])]}
+                vr_serializer = VoiceRecognitionSerializer(data=vr_data)
+                if vr_serializer.is_valid():
+                    vr_serializer.save()
+                else:
+                    return JsonResponse(vr_serializer.errors, status=400)
+                seq += 1
         return JsonResponse(results)
 
     elif request.method == 'DELETE':
-        voice_recognitions = VoiceRecognition.objects.filter(mn_id=mn_id).delete()
+        VoiceRecognition.objects.filter(mn_id=mn_id).delete()
         # Job 삭제하기
         my_config = Config(
             region_name='ap-northeast-2',
@@ -151,7 +184,7 @@ def voice_recognition_list(request, mn_id):
         )
         del_transcribe = boto3.client('transcribe', config=my_config)
         res = del_transcribe.delete_transcription_job(
-            TranscriptionJobName='{}_{}'.format(request.user.id, file.file_name)
+            TranscriptionJobName='{}_{}.{}'.format(file.file_id, file.file_name, file.file_extension)
         )
         res['ResponseMetadata']['HTTPStatusCode'] == '200'
         return HttpResponse(status=204)
@@ -171,7 +204,7 @@ def voice_recognition(request, mn_id, vr_seq):
         data = JSONParser().parse(request)
         vr_data = {'mn_id': mn_id, 'vr_seq': vr_seq, 'vr_start': data.get("vr_start"), 'vr_end': data.get("vr_end"),
                    'vr_text': data.get("vr_text"), 'speaker_seq': data.get("speaker_seq")}
-        serializer = VoiceRecognitionSerializer(data=data)
+        serializer = VoiceRecognitionSerializer(data=vr_data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
@@ -223,8 +256,8 @@ def voice_recognition_search(request, mn_id):
 def emotion_recognition(request, mn_id):
     obj = Minutes.objects.get(mn_id=mn_id)
     file_name = str(obj.file_id.file_name)
-    file_extention = str(obj.file_id.file_extension)
-    file = file_name + "." + file_extention
+    file_extension = str(obj.file_id.file_extension)
+    file = file_name + "." + file_extension
     data = VoiceRecognition.objects.filter(mn_id=mn_id)
     serializer = VoiceRecognitionSerializer(data, many=True)
 
